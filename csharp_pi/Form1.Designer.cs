47129ae73dab8913397d795465d22b321b7e84b5
JSpiner <jspiner@naver.com>
2016-06-09 15:29:42 +0900

    complete

--- csharp_pi/Form1.Designer.cs
+++ csharp_pi/Form1.Designer.cs
@@ -29,11 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(65, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 207);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 373);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- csharp_pi/Form1.cs
+++ csharp_pi/Form1.cs
@@ -11,9 +11,94 @@ namespace csharp_pi
 {
     public partial class Form1 : Form
     {
+
+        int R = 100;
+        Pen pen = new Pen(Brushes.DeepSkyBlue, 5);
+        List<Pos> posList;
+        Random rnd = new Random();
+
+        class Pos{
+            public int x;
+            public int y;
+
+            public Pos(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Bitmap canvas = new Bitmap(500, 500);
+            pictureBox1.Image = canvas;
+
+            Graphics g = Graphics.FromImage(canvas);
+
+            g.DrawEllipse(Pens.Black, new Rectangle(0, 0, 200, 200));
+            g.DrawLine(Pens.Black, new Point(100, 0), new Point(100, 200));
+            g.DrawLine(Pens.Black, new Point(0, 100), new Point(200, 100));
+
+            posList = new List<Pos>();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Pos pos = new Pos(
+                rnd.Next(0, R * 2), 
+                rnd.Next(0, R * 2));
+
+            drawPos(pos);
+
+            posList.Add(pos);
+
+            calcPI();
+        }
+
+        void calcPI()
+        {
+
+            int count = 0;
+            int i;
+            for (i = 0; i < posList.Count;i++ )
+            {
+                double distance =
+                    Math.Sqrt(
+                        Math.Pow(posList[i].x - R, 2) +
+                        Math.Pow(posList[i].y - R, 2)
+                    );
+                if (distance <= R) count++;
+            }
+            label1.Text = "PI : " + ((double) count / posList.Count * 4); 
+            /*
+             원 : 사각형 = PI * r * r : 2r * 2r = PI : 4
+             PI = 4 * 원안의 갯수 / 전체 갯수 
+              
+             */ 
+            label2.Text = "원 안의 점 수 : " + count + "\n"
+                            + "사각형 안의 점 수 : " + posList.Count;
+
+        }
+
+        void drawPos(Pos pos)
+        {
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            g.DrawRectangle(Pens.Black, new Rectangle(pos.x / (R / 100) ,pos.y / (R / 100), 2,2));
+
+            pictureBox1.Invalidate();
         }
     }
 }
